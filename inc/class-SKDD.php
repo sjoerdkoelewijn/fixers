64ae46604a69c4e6bf59dbb642303af1db727d40
Sjoerd <hello@sjoerdkoelewijn.com>
2021-06-18 17:38:49 +0200

    lazyload on domready

--- inc/class-SKDD.php
+++ inc/class-SKDD.php
@@ -528,7 +528,15 @@ if ( ! class_exists( 'SKDD' ) ) {
 				true
 			);
 
-		
+			// Lazy load.
+			wp_enqueue_script(
+				'SKDD-lazy-load',
+				SKDD_THEME_URI . 'assets/js/lazyload' . SKDD_suffix() . '.js',
+				array(),
+				SKDD_version(),
+				true
+			);
+			
 			// Mobile menu.
 			wp_enqueue_script(
 				'SKDD-navigation',
