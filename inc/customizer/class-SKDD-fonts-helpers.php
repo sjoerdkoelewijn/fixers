89bc0644dfd4b059d46b6b517720b4c8c747e23e
Sjoerd <s.koelewijn@gmail.com>
2021-08-20 17:21:50 +0200

    Update class-SKDD-fonts-helpers.php

--- inc/customizer/class-SKDD-fonts-helpers.php
+++ inc/customizer/class-SKDD-fonts-helpers.php
@@ -292,12 +292,8 @@ if ( ! class_exists( 'SKDD_Fonts_Helpers' ) ) :
 			}
 
 			// Create our URL using the arguments.
-
-			if ( class_exists( 'OMGF' ) ) {
-				$fonts_url = '/wp-content/uploads/omgf/SKDD-fonts/SKDD-fonts.css';
-			} else {
-				$fonts_url = add_query_arg( $font_args, '//fonts.googleapis.com/css' );
-			}			
+			$fonts_url = add_query_arg( $font_args, '//fonts.googleapis.com/css' );
+					
 
 			// Enqueue our fonts.
 			if ( $google_fonts ) {
