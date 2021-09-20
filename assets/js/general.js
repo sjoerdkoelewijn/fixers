11c30cb3870b907a9b9c80eb4a4b637f0d272a21
Sjoerd <s.koelewijn@gmail.com>
2021-09-20 16:58:28 +0200

    check if ghostkitCarousel is not null

--- assets/js/general.js
+++ assets/js/general.js
@@ -277,31 +277,35 @@ function shuffleCarousel() {
 
 	var ghostkitCarousel = document.querySelector('.ghostkit-carousel');
 
-	if (ghostkitCarousel.classList.contains('is-style-order-random')) {
+	if (ghostkitCarousel !== null) {
 
-		var elems = document.querySelectorAll('.ghostkit-carousel-items > .ghostkit-carousel-slide');
-	
-		var allElems = (function(){
-			var ret = [], l = elems.length;
-			while (l--) { ret[ret.length] = elems[l]; }
-			return ret;
-		})();
-	
-		var shuffled = (function(){
-			var l = allElems.length, ret = [];
+		if (ghostkitCarousel.classList.contains('is-style-order-random')) {
+
+			var elems = document.querySelectorAll('.ghostkit-carousel-items > .ghostkit-carousel-slide');
+		
+			var allElems = (function(){
+				var ret = [], l = elems.length;
+				while (l--) { ret[ret.length] = elems[l]; }
+				return ret;
+			})();
+		
+			var shuffled = (function(){
+				var l = allElems.length, ret = [];
+				while (l--) {
+					var random = Math.floor(Math.random() * allElems.length),
+						randEl = allElems[random].cloneNode(true);
+					allElems.splice(random, 1);
+					ret[ret.length] = randEl;
+				}
+				return ret; 
+			})(), l = elems.length;
+		
 			while (l--) {
-				var random = Math.floor(Math.random() * allElems.length),
-					randEl = allElems[random].cloneNode(true);
-				allElems.splice(random, 1);
-				ret[ret.length] = randEl;
+				elems[l].parentNode.insertBefore(shuffled[l], elems[l].nextSibling);
+				elems[l].parentNode.removeChild(elems[l]);
 			}
-			return ret; 
-		})(), l = elems.length;
-	
-		while (l--) {
-			elems[l].parentNode.insertBefore(shuffled[l], elems[l].nextSibling);
-			elems[l].parentNode.removeChild(elems[l]);
 		}
+
 	}
  
 }
