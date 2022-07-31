30790e5c40835230081952bf0d9e3206a5d24176
Sjoerd <s.koelewijn@gmail.com>
2022-07-31 17:47:12 +0200

    Update custom-post-types.php

--- inc/customizer/sections/posttypes/custom-post-types.php
+++ inc/customizer/sections/posttypes/custom-post-types.php
@@ -156,6 +156,176 @@ $wp_customize->add_control(
 // ----------------------------------------------------------------------------------------------------------
 
 $wp_customize->add_setting(
+	'location_divider',
+	array(
+		'sanitize_callback' => 'sanitize_text_field',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Divider_Control(
+		$wp_customize,
+		'location_divider',
+		array(
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'location_divider',
+			'type'     => 'divider',
+		)
+	)
+);
+
+// Show location cpt
+$wp_customize->add_setting(
+	'SKDD_setting[cpt_location_display]',
+	array(
+		'type'              => 'option',
+		'default'           => $defaults['cpt_location_display'],
+		'sanitize_callback' => 'SKDD_sanitize_checkbox',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Switch_Control(
+		$wp_customize,
+		'SKDD_setting[cpt_location_display]',
+		array(
+			'label'    => __( 'Location Post Type', 'SKDD' ),
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'SKDD_setting[cpt_location_display]',
+		)
+	)
+);
+
+// Has archive page 
+$wp_customize->add_setting(
+	'SKDD_setting[cpt_location_has_archive]',
+	array(
+		'type'              => 'option',
+		'default'           => $defaults['cpt_location_has_archive'],
+		'sanitize_callback' => 'SKDD_sanitize_checkbox',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Switch_Control(
+		$wp_customize,
+		'SKDD_setting[cpt_location_has_archive]',
+		array(
+			'label'    => __( 'Has archive page', 'SKDD' ),
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'SKDD_setting[cpt_location_has_archive]',
+		)
+	)
+);
+
+// Has categories 
+$wp_customize->add_setting(
+	'SKDD_setting[cpt_location_has_tax]',
+	array(
+		'type'              => 'option',
+		'default'           => $defaults['cpt_location_has_tax'],
+		'sanitize_callback' => 'SKDD_sanitize_checkbox',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Switch_Control(
+		$wp_customize,
+		'SKDD_setting[cpt_location_has_tax]',
+		array(
+			'label'    => __( 'Add categories', 'SKDD' ),
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'SKDD_setting[cpt_location_has_tax]',
+		)
+	)
+);
+
+
+
+// ----------------------------------------------------------------------------------------------------------
+
+$wp_customize->add_setting(
+	'support_divider',
+	array(
+		'sanitize_callback' => 'sanitize_text_field',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Divider_Control(
+		$wp_customize,
+		'support_divider',
+		array(
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'support_divider',
+			'type'     => 'divider',
+		)
+	)
+);
+
+// Show location cpt
+$wp_customize->add_setting(
+	'SKDD_setting[cpt_support_display]',
+	array(
+		'type'              => 'option',
+		'default'           => $defaults['cpt_support_display'],
+		'sanitize_callback' => 'SKDD_sanitize_checkbox',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Switch_Control(
+		$wp_customize,
+		'SKDD_setting[cpt_support_display]',
+		array(
+			'label'    => __( 'Support Post Type', 'SKDD' ),
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'SKDD_setting[cpt_support_display]',
+		)
+	)
+);
+
+// Has archive page 
+$wp_customize->add_setting(
+	'SKDD_setting[cpt_support_has_archive]',
+	array(
+		'type'              => 'option',
+		'default'           => $defaults['cpt_support_has_archive'],
+		'sanitize_callback' => 'SKDD_sanitize_checkbox',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Switch_Control(
+		$wp_customize,
+		'SKDD_setting[cpt_support_has_archive]',
+		array(
+			'label'    => __( 'Has archive page', 'SKDD' ),
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'SKDD_setting[cpt_support_has_archive]',
+		)
+	)
+);
+
+// Has categories 
+$wp_customize->add_setting(
+	'SKDD_setting[cpt_support_has_tax]',
+	array(
+		'type'              => 'option',
+		'default'           => $defaults['cpt_support_has_tax'],
+		'sanitize_callback' => 'SKDD_sanitize_checkbox',
+	)
+);
+$wp_customize->add_control(
+	new SKDD_Switch_Control(
+		$wp_customize,
+		'SKDD_setting[cpt_support_has_tax]',
+		array(
+			'label'    => __( 'Add categories', 'SKDD' ),
+			'section'  => 'SKDD_custom_post_types',
+			'settings' => 'SKDD_setting[cpt_support_has_tax]',
+		)
+	)
+);
+
+
+
+// ----------------------------------------------------------------------------------------------------------
+
+$wp_customize->add_setting(
 	'knowledge_divider',
 	array(
 		'sanitize_callback' => 'sanitize_text_field',
