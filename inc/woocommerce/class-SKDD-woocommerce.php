e0abbcd6a59c88cd2c99cc8cc6f6f8e1def9b5cd
Sjoerd <s.koelewijn@gmail.com>
2022-09-02 12:18:53 +0200

    product price

--- inc/woocommerce/class-SKDD-woocommerce.php
+++ inc/woocommerce/class-SKDD-woocommerce.php
@@ -495,14 +495,14 @@ if ( ! class_exists( 'SKDD_WooCommerce' ) ) {
 			remove_action( 'woocommerce_before_single_product_summary', 'woocommerce_show_product_sale_flash', 10 );
 
 			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
-			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+			//remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
 			remove_action( 'woocommerce_add_to_cart', 'action_woocommerce_add_to_cart', 10 ); 
 
 			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
 
 			remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 
-			add_filter( 'woocommerce_get_price_html', 'custom_template_single_product_weight' );
+			//add_filter( 'woocommerce_get_price_html', 'custom_template_single_product_weight' );
 
 			//add_filter( 'woocommerce_product_tabs', 'SKDD_change_tabs_order', 98 );
 
